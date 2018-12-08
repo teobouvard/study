a0da22bae84c509c4c99ca987a2770a306d6f71d
teobouvard <teobouvard@liver.fr>
2018-12-08 15:48:03 +0100

    deleted useless copy ctors

--- src/Trajet.h
+++ src/Trajet.h
@@ -9,19 +9,42 @@ copyright            : Mathis Guilhin & Téo Bouvard
 #ifndef Trajet_H
 #define Trajet_H
 //--------------------------------------------------- Interfaces utilisées
+// Rôle de la classe <Trajet>
+/*
+La classe <Trajet> est une classe abstraite qui représente un Trajet comme un
+objet ayant une ville de départ et une ville d'arrivée.
+*/
 
 class Trajet
 {
 	//----------------------------------------------------------------- PUBLIC
-
 public:
 	//----------------------------------------------------- Méthodes publiques
+
+	//méthode virtuelle pure qui permet d'afficher les attributs d'un trajet
 	virtual void Afficher() const = 0;
+
+	/*
+	- méthode qui renvoie la ville de départ (depart_arrivee = 0) ou d'arrivée
+	(depart_arrivee = 1) d'un trajet
+	- utilisée dans les méthodes de recherche du Catalogue, et l'affichage des
+	trajets composés
+	*/
 	char* getVille(int depart_arrivee) const;
+
+	/*
+	- méthode virtuelle pure qui permet de cloner un trajet
+	- utilisée dans les méthodes de recherche du Catalogue, le clonage d'une Collection
+	et implémentée dans TrajetSimple et TrajetCompose
+	*/
 	virtual Trajet* clone() const = 0;
 
 	//-------------------------------------------- Constructeurs - destructeur
+
+	//construit un trajet grâce à une ville de départ et une ville d'arrivée
 	Trajet (const char * villeDep, const char * villeArr);
+
+	//détruit les deux pointeurs attributs de la classe
 	virtual ~Trajet ( );
 
 	//------------------------------------------------------------------ PRIVE
--- src/TrajetSimple.h
+++ src/TrajetSimple.h
@@ -5,13 +5,17 @@ début                : Novembre 2018
 copyright            : Mathis Guilhin & Téo Bouvard
 *************************************************************************/
 
-//---------- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----------------
+//---------- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ---
 #ifndef TrajetSimple_H
 #define TrajetSimple_H
 //--------------------------------------------------- Interfaces utilisées
 #include "Trajet.h"
 //------------------------------------------------------------------------
 // Rôle de la classe <TrajetSimple>
+/*
+La classe <TrajetSimple> est une sorte de Trajet qui comporte un mode de Transport
+
+*/
 
 class TrajetSimple : public Trajet
 {
@@ -22,16 +26,18 @@ public:
   virtual Trajet* clone() const;
 
   //-------------------------------------------- Constructeurs - destructeur
-  TrajetSimple ( const TrajetSimple & unTrajetSimple );
+
+  /*construit un Trajet Simple grâce à une ville de départ, une ville d'arrivée
+  et un mode de transport*/
   TrajetSimple ( const char * villeDep, const char * villeArr, const char * modeTrans );
+
+  //détruit le pointeur attribut de la classe
   virtual ~TrajetSimple ( );
 
   //------------------------------------------------------------------ PRIVE
 protected:
-  //----------------------------------------------------- Méthodes protégées
-  char * modeTransport;
   //----------------------------------------------------- Attributs protégés
-
+  char * modeTransport;
 };
 
 #endif // TrajetSimple_H

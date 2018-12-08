3357283e96ec7b378db96a46eae6d5ea60e28132
teobouvard <teobouvard@liver.fr>
2018-12-08 17:50:54 +0100

    description Catalogue

--- src/Catalogue.h
+++ src/Catalogue.h
@@ -14,32 +14,55 @@ copyright            : Mathis Guilhin & Téo Bouvard
 //-----------------------------------------------------------------------------
 /* Rôle de la classe <Catalogue>
 
-La classe <Catalogue> correspond à la structure de données utilisée pour ce TP.
-Elle est constituée d'un tableau dynamique de pointeurs sur des trajets. La taille
-actuelle du tableau dynamique est stockée dans l'attribut tailleTableau, et le
-nombre de trajets qu'elle contient dans l'attribut nbElements.
+La classe <Catalogue> représente un catalogue de trajets, et se base sur la
+structure de données définie par la classe Collection.
 */
-//------------------------------------------------------------------------
+
 class Catalogue
 {
   //----------------------------------------------------------------- PUBLIC
 public:
   //----------------------------------------------------- Méthodes publiques
 
+  /*
+  affiche le nombre de trajets présents dans le catalogue, ainsi que les différents
+  trajets qui le composent
+  */
   void AfficherCatalogue();
+
+  /*
+  recherche dans le catalogue tous les trajets qui ont comme ville de départ le
+  paramètre depart et comme ville d'arrivée le paramètre arrivee
+  */
   void RechercherTrajet(char* depart, char* arrivee) const;
+
+  /*
+  - recherche dans le catalogue tous les trajets qui ont comme ville de départ le
+  paramètre depart et comme ville d'arrivée le paramètre arrivee, en composant
+  les trajets
+  - cette méthode est récursive, le paramètre profondeurRecherche permet de savoir
+    à quel étage de la recherche on se situe
+  - le paramètre c est une collection de trajets qui correspond à une des compositions
+    de trajets permettant d'arriver à destination
+  */
   void RechercherTrajetAvance(char* depart, char* arrivee, int profondeurRecherche, Collection* c) const;
+
+  /*
+  - retourne la collection de trajets du Catalogue
+  - utilisé dans la classe de test Voyages
+  */
   Collection* getCollection();
 
-  //procédure utilisée pour réinitialiser l'affichage du nombre d'options dans les recherches
+  //procédure utilisée pour réinitialiser l'affichage du nombre d'options après une recherche
   void RaZ_nbOption();
-
-  void addOneTrajet();
 
   //-------------------------------------------- Constructeurs - destructeur
 
-  Catalogue ( );
-  virtual ~Catalogue ( );
+  //construction d'un catalogue vide
+  Catalogue();
+
+  //destruction du pointeur sur collection, seul attribut de la classe
+  virtual ~Catalogue();
 
   //------------------------------------------------------------------ PRIVE
 protected:

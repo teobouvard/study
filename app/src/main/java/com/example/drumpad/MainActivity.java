2d00300516f8608d3616add17a240aa3e9709b0e
teobouvard <teobouvard@liver.fr>
2018-12-20 02:28:48 +0100

    sound stops working after 15 media players ?

--- app/src/main/java/com/example/drumpad/MainActivity.java
+++ app/src/main/java/com/example/drumpad/MainActivity.java
@@ -7,71 +7,73 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mp0,mp1,mp2,mp3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mp0 = MediaPlayer.create(this, R.raw.clap0);
+        mp1 = MediaPlayer.create(this, R.raw.clapslapper);
+        mp2 = MediaPlayer.create(this, R.raw.crash808);
+        mp3 = MediaPlayer.create(this, R.raw.crashnoise);
     }
 
 
     public void button0Click(View view){
-        final MediaPlayer mp0 = MediaPlayer.create(this, R.raw.clap0);
         mp0.start();
     }
 
     public void button1Click(View view){
-        final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.clap0);
         mp1.start();
     }
 
     public void button2Click(View view){
-        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.clap0);
         mp2.start();
     }
 
     public void button3Click(View view){
-        final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.clap0);
         mp3.start();
     }
 
     public void button4Click(View view){
-        final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.hihatacoustic01);
         mp4.start();
     }
 
     public void button5Click(View view){
-        final MediaPlayer mp5 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp5 = MediaPlayer.create(this, R.raw.kickacoustic02);
         mp5.start();
     }
 
     public void button6Click(View view){
-        final MediaPlayer mp6 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp6 = MediaPlayer.create(this, R.raw.kickdeep);
         mp6.start();
     }
 
     public void button7Click(View view){
-        final MediaPlayer mp7 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp7 = MediaPlayer.create(this, R.raw.kickvinyl01);
         mp7.start();
     }
 
     public void button8Click(View view){
-        final MediaPlayer mp8 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp8 = MediaPlayer.create(this, R.raw.openhat808);
         mp8.start();
     }
 
     public void button9Click(View view){
-        final MediaPlayer mp9 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp9 = MediaPlayer.create(this, R.raw.snareacoustic01);
         mp9.start();
     }
 
     public void button10Click(View view){
-        final MediaPlayer mp10 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp10 = MediaPlayer.create(this, R.raw.rideacoustic02);
         mp10.start();
     }
 
     public void button11Click(View view){
-        final MediaPlayer mp11 = MediaPlayer.create(this, R.raw.clap0);
+        final MediaPlayer mp11 = MediaPlayer.create(this, R.raw.crashacoustic);
         mp11.start();
     }
 

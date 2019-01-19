9d85083d5f2d17d8c982c877c898a364e4772d07
teobouvard <teobouvard@gmail.com>
2019-01-19 18:23:05 +0100

    animation ok

--- Filters/PasseBasBinaire.m
+++ Filters/PasseBasBinaire.m
@@ -1,45 +1,70 @@
-function passeBasBinaire
+function passeBasBinaire(img)
 
-k = 1000;
+    close all;
 
-im = imread('imtest02.png');
-[l,c,~]=size(im);
-
-map=([0:255]'/255)*[1 1 1];
-
-IM = fftshift(fft2(im));%on passe en frequentiel
-IMaff = (log(abs(IM)+1));%création d'une version affichable de la représentation fréquentielle
-
-binaryFilter = zeros(l,c); %création du filtre
-IMF = zeros(l,c); %création de la représentation fréquentielle filtrée
-
-for u = 1:l
-    for v = 1:c
-        if (((u-l/2+1)^2+(v-c/2+1)^2) < k)
-             binaryFilter(u,v) = 1;
-        end
-        IMF(u,v)=IM(u,v)*binaryFilter(u,v);
+    %lecture des images test
+    if img == 0
+    im = imread('imtest01.png');
+    else
+    im = imread('imtest02.png');
     end
+
+    [l,c,~]=size(im);
+
+    %creation d'un mapping couleur linéaire sur chaque flux RGB
+    map=([0:255]'/255)*[1 1 1];
+
+    %calcul du spectre frequentiel 
+    spectre_freq = fftshift(fft2(im));
+
+    %création d'une version affichable de la représentation fréquentielle
+    spectre_freq_aff = (log(abs(spectre_freq))+1);
+
+    %initialisation du filtre
+    binaryFilter = zeros(l,c);
+
+    for r = 1:2:180
+        
+        r
+        
+        for u = 1:l
+            for v = 1:c
+                if (((u-l/2+1)^2+(v-c/2+1)^2) < r^2)
+                    binaryFilter(u,v) = 1;
+                end
+            end
+        end
+
+        %filtrage fréquentiel
+        spectre_filtre = spectre_freq .* binaryFilter;
+
+        %version affichable
+        spectre_filtre_aff = (log(abs(spectre_filtre)+1));
+
+        %reorganisation representation frequentielle
+        spectre_filtre = fftshift(spectre_filtre);
+
+        %calcul de l'image filtrée par transformée de fourier inverse
+        im_filtre = ifft2(spectre_filtre);
+
+        %affichage
+        subplot(2,2,1);
+        image(im);
+        colormap(map);
+
+        subplot(2,2,2);
+        imagesc(spectre_freq_aff);
+
+        subplot(2,2,3);
+        image(abs(im_filtre));
+        colormap(map);
+
+        subplot(2,2,4);
+        imagesc(spectre_filtre_aff);
+
+        waitforbuttonpress;
+
+    end
+
+    close all;
 end
-
-IMFaff = (log(abs(IMF)+1));
-
-IMF = fftshift(IMF);%reorganisation representation frequentielle
-imf = ifft2(IMF);%on passe en spatial
-
-
-figure('units','normalized','outerposition',[0 0 1 1]);
-
-subplot(2,2,1);
-image(im);
-colormap(map);
-
-subplot(2,2,2);
-imagesc(IMaff);
-
-subplot(2,2,3);
-image(abs(imf));
-colormap(map);
-
-subplot(2,2,4);
-imagesc(IMFaff);
--- Filters/filtrageTest.m
+++ Filters/filtrageTest.m
@@ -1,28 +1,52 @@
 function filtrageTest(k)
 
-im = imread('Stephane_Bres2.jpg');
-[l,c,m]=size(im);
-im = 0.3*im(:,:,1) + 0.59*im(:,:,2) + 0.11*im(:,:,3);
+    im = rgb2gray(imread('Stephane_Bres.jpg'));
+    [l,c,~]=size(im);
 
-IM = fftshift(fft2(im));%on passe en frequentiel
-IMaff = (log(abs(IM)+1));%reorganisation representation frequentielle
+    %creation d'un mapping couleur linéaire sur chaque flux RGB
+    map=([0:255]'/255)*[1 1 1];
 
-gaussMatrix = zeros(l,c);
-IMF = zeros(l,c);
+    %calcul du spectre frequentiel 
+    spectre_freq = fftshift(fft2(im));
 
-for u = 1:l
-    for v = 1:c
-        gaussMatrix(u,v)=exp(-k*((u-l/2+1)^2+(v-c/2+1)^2));
-        IMF(u,v)=IM(u,v)*gaussMatrix(u,v);
+    %initialisation du filtre
+    gaussMatrix = zeros(l,c);
+
+    for n = 0.1:0.1:5
+
+        k = -power(10,-n);
+
+        for u = 1:l
+            for v = 1:c
+                gaussMatrix(u,v)=exp(k*((u-l/2+1)^2+(v-c/2+1)^2));
+            end
+        end
+
+        %filtrage fréquentiel
+        spectre_filtre = spectre_freq .* gaussMatrix;
+
+        %version affichable
+        spectre_filtre_aff = (log(abs(spectre_filtre)+1));
+
+        %reorganisation representation frequentielle
+        spectre_filtre = fftshift(spectre_filtre);
+
+        %calcul de l'image filtrée par transformée de fourier inverse
+        im_filtre = ifft2(spectre_filtre);
+
+        %affichage
+        subplot(1,2,1);
+        image(abs(im_filtre));
+        pbaspect([1 1 1]);
+        colormap(map);
+
+        subplot(1,2,2);
+        imagesc(spectre_filtre_aff);
+        pbaspect([1 1 1]);
+
+        waitforbuttonpress;
     end
+
+    close all;
 end
 
-
-IMF = fftshift(IMF);%reorganisation representation frequentielle
-imf = ifft2(IMF);%on passe en spatial
-(max(abs(imag(imf))))
-mapbw=([0:255]'/255)*[1 1 1];
-figure(1)
-%imagesc(log(abs(fftshift(IMBW))))
-image((abs(imf)));
-colormap(mapbw);

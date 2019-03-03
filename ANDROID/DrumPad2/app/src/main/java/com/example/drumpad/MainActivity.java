284a99fc01358142105f8c3b6e73c88cc709245e
FelixFonteneau <34739390+FelixFonteneau@users.noreply.github.com>
2019-03-03 13:48:51 +0100

    up boutton

--- ANDROID/DrumPad2/app/src/main/java/com/example/drumpad/MainActivity.java
+++ ANDROID/DrumPad2/app/src/main/java/com/example/drumpad/MainActivity.java
@@ -1,40 +1,41 @@
 package com.example.drumpad;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
 
+import static android.view.MotionEvent.ACTION_DOWN;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.clapslapper);
-        final MediaPlayer mediaPlayer2 = MediaPlayer.create(this, R.raw.crash808);
-
-        final Button btn = (Button) this.findViewById(R.id.buttonTest);
-        final Button btn2 = (Button) this.findViewById(R.id.buttonTest2);
-        final Toast t = Toast.makeText(this,"ma queue",Toast.LENGTH_SHORT);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                t.show();
-                mediaPlayer.start();
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer2.start();
-            }
-        });
-
+        setupButtons();
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupButtons() {
+        final Boutton boutton = new Boutton(this,"buttonTest","clapslapper",1);
+
+        boutton.getButton().setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getActionMasked()==ACTION_DOWN){
+                    boutton.stopPlaying();
+                    boutton.setPlayerSound();
+                    boutton.getMediaPlayer().start();
+                }
+                return false;
+            }
+        });
+    }
+
 }

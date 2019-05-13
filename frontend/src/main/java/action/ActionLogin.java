157e2de59a42b1acc45963bcd5f10ac13edecf15
Téo Bouvard <teobouvard@gmail.com>
2019-05-13 21:26:23 +0200

    cleanup working tree

---
+++ frontend/src/main/java/action/ActionLogin.java
@@ -0,0 +1,34 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import metier.data.Client;
+import metier.data.Personne;
+import metier.service.Service;
+
+public class ActionLogin extends Action {
+
+    @Override
+    public boolean executer(HttpServletRequest request) {
+        Integer login = Integer.parseInt(request.getParameter("login"));
+        String password = request.getParameter("password");
+
+        Personne personne = Service.chercherPersonne(login, password);
+
+        if (personne != null) {
+            System.out.println("Personne trouvee");
+            request.setAttribute("statut", Boolean.TRUE);
+            HttpSession session = request.getSession();
+            session.setAttribute("personne", personne);
+            if (personne instanceof Client) {
+                request.setAttribute("personne", "client");
+            } else {
+                request.setAttribute("personne", "employe");
+            }
+        } else {
+            System.out.println("Personne non trouvee");
+            request.setAttribute("statut", Boolean.FALSE);
+        }
+        return true;
+    }
+}
---
+++ frontend/src/main/java/serialisation/SerialisationInscrire.java
@@ -0,0 +1,32 @@
+package serialisation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SerialisationInscrire extends Serialisation {
+
+    @Override
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = getWriterWithJsonHeader(response);
+        JsonObject jsonContainer = new JsonObject();
+        Boolean statut = (Boolean) request.getAttribute("statut");
+
+        if (statut) {
+            jsonContainer.addProperty("inscription", true);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(jsonContainer);
+            out.println(json);
+        } else {
+            jsonContainer.addProperty("inscription", false);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(jsonContainer);
+            out.println(json);
+        }
+    }
+}

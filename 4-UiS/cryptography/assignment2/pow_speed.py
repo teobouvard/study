e63f74e32391ac41bd3d536b4d90f3155dee1b5d
Téo Bouvard <teobouvard@gmail.com>
2019-10-02 15:46:08 +0200

    Add matplotlib visualization of pow speed

--- 4-UiS/cryptography/assignment2/pow_speed.py
+++ 4-UiS/cryptography/assignment2/pow_speed.py
@@ -1,6 +1,7 @@
+from matplotlib import pyplot as plt
 import timeit
 
-LIMIT = 10e10
+LIMIT = 10e5
 x = 1
 a = 2
 m = 2
@@ -12,7 +13,9 @@ x_list = []
 while x < LIMIT:
     x_list.append(x)
     raw_times.append(timeit.timeit('{} ** {} % {}'.format(a, x, m), number=1))
-    pow_times.append(timeit.timeit('pow({}, {}, {})'.format(a, x, m)))
+    pow_times.append(timeit.timeit('pow({}, {}, {})'.format(a, x, m), number=1))
     x *= 10
 
-print(pow_times, raw_times)
+    plt.plot(x_list, raw_times)
+    plt.plot(x_list, pow_times)
+    plt.show()

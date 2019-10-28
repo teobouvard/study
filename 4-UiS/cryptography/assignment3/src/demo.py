3dd9ceea50d8db978018240583919b6551ba6835
Téo Bouvard <teobouvard@gmail.com>
2019-10-28 21:43:29 +0100

    Work on report

---
+++ 4-UiS/cryptography/assignment3/src/demo.py
@@ -0,0 +1,22 @@
+import random
+
+from blockchain import Block, Blockchain, Transaction
+
+USERS = ['Alice', 'James', 'Smith', 'David', 'Adams', 'Isaac', 'Lewis']
+
+
+if __name__ == '__main__':
+
+    chain = Blockchain()
+
+    for _ in range(random.randint(1, 5)):
+        block = Block()
+
+        for _ in range(random.randint(1, 3)):
+            sender, receiver = random.sample(USERS, k=2)
+            transaction = Transaction(sender=sender, receiver=receiver, value=random.randint(10, 100))
+            block.add_transaction(transaction)
+            #print(block)
+        chain.add_block(block)
+        print(chain)
+

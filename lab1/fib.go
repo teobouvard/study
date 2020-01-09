7d7a73be7c0ea97bb629d45ec25366902823de35
Téo Bouvard <teobouvard@gmail.com>
2020-01-09 15:14:32 +0100

    Add recursive fibonacci

--- lab1/fib.go
+++ lab1/fib.go
@@ -7,5 +7,11 @@ package lab1
 // fibonacci(n) returns the n-th Fibonacci number, and is defined by the
 // recurrence relation F_n = F_n-1 + F_n-2, with seed values F_0=0 and F_1=1.
 func fibonacci(n uint) uint {
-	return 0
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
 }

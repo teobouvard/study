cab2565e4f5cbd710afc3056a38cc64d41e07a4b
Téo Bouvard <teobouvard@gmail.com>
2020-02-03 12:44:57 +0100

    Add split class to factor splitting and predictions

--- assignment2/models.py
+++ assignment2/models.py
@@ -2,6 +2,13 @@ import numpy as np
 
 from multiprocessing import Pool
 from metrics import RMSE, log_RMSE
+import operator
+
+class Split:
+    def __init__(self, index, value, op):
+        self.index = index
+        self.value = value
+        self.op = op
 
 class Node:
 
@@ -13,21 +20,21 @@ class Node:
         self.value = y.mean() if self.size > 0 else 0
         self.error = RMSE(np.repeat(self.value, self.size), y)
         if not self.is_leaf and not benchmark:
-            self.split_idx, self.split_value, self.split_type = self.find_best_split(x, y)
+            self.split = self.find_best_split(x, y)
             self.left, self.right = self.grow(x, y)
     
     def find_best_split(self, x, y):
         split_idx = None
         split_value = None
-        split_type = None
+        split_op = None
         best_error_reduction = 0
 
         for idx in range(x.shape[1]):
-            # check that all feature values for this index are of the same type
-            if len(set(type(_) for _ in x[:, idx])) > 1:
-                raise ValueError(f'{set(type(_) for _ in x[:, idx])} : different types for same attribute at index {idx}')
-
             possible_values = sorted(set(x[:, idx]))
+
+            # check that all feature values for this index are of the same type
+            if len(set(type(_) for _ in possible_values)) > 1:
+                raise ValueError(f'{set(type(_) for _ in x[:, idx])} : different types for same attribute at index {idx}')
 
             # numerical attributes
             if isinstance(x[0, idx], (int, float)):
@@ -40,7 +47,7 @@ class Node:
                         best_error_reduction = error_reduction
                         split_idx = idx
                         split_value = threshold
-                        split_type = 'numeric'
+                        split_op = operator.le
 
             # categorical attributes
             elif isinstance(x[0, idx], str):
@@ -52,21 +59,16 @@ class Node:
                         best_error_reduction = error_reduction
                         split_idx = idx
                         split_value = value
-                        split_type = 'categorical'
-
+                        split_op = operator.eq
+                        
+            # invalid attributes
             else:
                 raise ValueError(f'Attribute {x[0, idx]} is of type {type(x[0, idx])} but should be int, float or str')
         
-        return split_idx, split_value, split_type
+        return Split(split_idx, split_value, split_op)
     
     def grow(self, x, y):
-        if self.split_type == 'numeric':
-            left_mask = x[:, self.split_idx] <= self.split_value
-        elif self.split_type == 'categorical':
-            left_mask = x[:, self.split_idx] == self.split_value
-        else:
-            raise ValueError(f'Split type is {self.split_type} but sould be numeric or categorical')
-
+        left_mask = self.split.op(x[:, self.split.index], self.split.value)
         left_node = Node(x[left_mask], y[left_mask], depth=self.depth+1, max_depth=self.max_depth)
         right_node = Node(x[np.invert(left_mask)], y[np.invert(left_mask)], depth=self.depth+1, max_depth=self.max_depth)
         return left_node, right_node
@@ -87,16 +89,10 @@ class DecisionTree:
     def predict_instance(self, x):
         node = self.tree
         while not node.is_leaf:
-            if node.split_type == 'numeric':
-                if x[node.split_idx] < node.split_value:
-                    node = node.left
-                else:
-                    node = node.right
-            elif node.split_type == 'categorical':
-                if x[node.split_idx] == node.split_value:
-                    node = node.left
-                else:
-                    node = node.right
+            if node.split.op(x[node.split.index], node.split.value):
+                node = node.left
+            else:
+                node = node.right
         return node.value
 
 
@@ -111,8 +107,6 @@ class RandomForest:
         ys = [_[1] for _ in subsamples]
         with Pool() as p:
             self.trees = p.starmap(DecisionTree.fit, zip(self.trees, xs, ys))
-        #for t in self.trees:
-        #    t.fit(*self.sample(x, y, self.subsample_size))
 
     def predict(self, x):
         predictions = np.array([t.predict(x) for t in self.trees])
@@ -152,18 +146,18 @@ if __name__ == '__main__':
     train_labels = train_data.pop('SalePrice')
     x_train, y_train, x_val, y_val = train_test_split(train_data, train_labels)
 
-    #model = RandomForest(n_trees=16, max_depth=100)
-    #model.fit(x_train.values, y_train.values)
-    #y_pred = model.predict(x_val.values)
-    #baseline_error = RMSE(np.repeat(y_train.values.mean(), len(y_val)), y_val.values)
-    #error = RMSE(y_pred, y_val.values)
-    #log_error = log_RMSE(y_pred, y_val.values)
-    #print(f'RMSE : {error} - l-RMSE : {log_error} - {error/baseline_error:%} of baseline error')
+    model = RandomForest(n_trees=16, max_depth=100)
+    model.fit(x_train.values, y_train.values)
+    y_pred = model.predict(x_val.values)
+    baseline_error = RMSE(np.repeat(y_train.values.mean(), len(y_val)), y_val.values)
+    error = RMSE(y_pred, y_val.values)
+    log_error = log_RMSE(y_pred, y_val.values)
+    print(f'RMSE : {error} - l-RMSE : {log_error} - {error/baseline_error:%} of baseline error')
 
-    model = RandomForest(n_trees=1000, max_depth=100)
-    model.fit(train_data.values, train_labels.values)
-    y_pred = model.predict(test_data.values)
-    submission = pd.DataFrame()
-    submission['Id'] = test_data.index
-    submission['SalePrice'] = y_pred
-    submission.to_csv('submission.csv', index=False)
+    #model = RandomForest(n_trees=100, max_depth=100)
+    #model.fit(train_data.values, train_labels.values)
+    #y_pred = model.predict(test_data.values)
+    #submission = pd.DataFrame()
+    #submission['Id'] = test_data.index
+    #submission['SalePrice'] = y_pred
+    #submission.to_csv('submission.csv', index=False)

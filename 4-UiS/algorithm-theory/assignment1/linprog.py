d36cbb1d2413c6d61deafa567f65d701d56b1a3c
Téo Bouvard <teobouvard@gmail.com>
2020-02-06 20:12:56 +0100

    Finished assignment 1

--- 4-UiS/algorithm-theory/assignment1/linprog.py
+++ 4-UiS/algorithm-theory/assignment1/linprog.py
@@ -1,6 +1,23 @@
 from pulp import *
 
-problem = LpProblem(LpMaximize)
+# problem definition
+prob = LpProblem(sense=LpMaximize)
 
+# variables
 x_1 = LpVariable('x_1', lowBound=0)
 x_2 = LpVariable('x_2', lowBound=0)
+
+# objective function
+prob += 3*x_1 + 5*x_2
+
+# constraints
+prob += x_1 + 2*x_2 <= 50
+prob += 8*x_1 + 3*x_2 <= 240
+
+GLPK().solve(prob)
+
+# solution
+for v in prob.variables():
+    print(f'{v.name} = {v.varValue}')
+
+print(f'objective = {value(prob.objective)}')

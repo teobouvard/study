f2aa3f3a56e3f8df1b7459a28db6e380309f042e
Téo Bouvard <teobouvard@gmail.com>
2020-02-12 19:09:38 +0100

    Implement LMS algorithm

--- 4-UiS/machine-learning/assignments/theory4/graphs.py
+++ 4-UiS/machine-learning/assignments/theory4/graphs.py
@@ -19,7 +19,6 @@ def problem_1():
     y = 11/2 - 2*x
     ax.plot(x, y, 'k-', label='g')
 
-
     ax.legend()
     fig.savefig('graph2.png')
 
@@ -29,6 +28,41 @@ def problem_1():
     ax.legend()
     fig.savefig('graph3.png')
 
+def problem_2():
+    learning_rate = 0.5
+    theta_i = 1
+    theta = [1, 1, 1]
+    threshold = 1
+    y = [1, 1, 2, 2]
+    N = len(y)
+    i = 0
+    while learning_rate*() > threshold:
+        i = (i+1)%N
+        learning_rate = learning_rate/i
+        theta = theta 
+
+
+def problem_2():
+    learning_rate = 0.5
+    theta = np.array([1, 1, 1])
+    threshold = 0.001
+    y = np.array([-1, -1, 1, 1])
+    x = np.array([[1, 2, 1], [2, 0, 1], [3, 1, 1], [2, 3, 1]])
+    N = len(y)
+    i = 1
+    sample_index = 0
+    error = np.linalg.norm(learning_rate*(y[sample_index] - theta.T * y[sample_index] @ x[sample_index])*(y[sample_index] * x[sample_index]))
+    while error > threshold:
+        i += 1
+        sample_index = i % N
+        learning_rate = learning_rate/i
+        descent = learning_rate*(y[sample_index] - theta.T * y[sample_index] @ x[sample_index])*(y[sample_index] * x[sample_index])
+        theta = theta + descent
+        error = np.linalg.norm(descent)
+        print(f'Error : {error}')
+        print(f'Theta : {theta}')
+
 
 if __name__ == '__main__':
-    problem_1()
+    #problem_1()
+    problem_2()

dd639d16028bfd84c6350de2e683d87a49fb19e9
Téo Bouvard <teobouvard@gmail.com>
2020-03-24 18:09:29 +0100

    End of ml lab 5

--- 4-UiS/machine-learning/assignments/lab5/models.py
+++ 4-UiS/machine-learning/assignments/lab5/models.py
@@ -2,10 +2,11 @@ from sklearn.neighbors import KernelDensity
 import numpy as np
 
 class ParzenClassifier:
-    def __init__(self, h, kernel='gaussian'):
+    def __init__(self, h, kernel):
         self.classes = []
         self.h = h
         self.distributions = {}
+        self.priors = {}
 
         if kernel not in ['gaussian','tophat','epanechnikov','exponential','linear','cosine']:
             raise NotImplementedError('Not a valid kernel')
@@ -15,7 +16,8 @@ class ParzenClassifier:
         class_labels = np.unique(y)
         for label in class_labels:
             self.classes.append(label)
-            class_idx = np.where(y == label)
+            class_idx = (y == label)
+            self.priors[label] = class_idx.sum()/len(y)
             hn = self.h / np.sqrt(len(class_idx))
             dist = KernelDensity(bandwidth=hn, kernel=self.kernel)
             self.distributions[label] = dist.fit(x[class_idx], y[class_idx])
@@ -24,4 +26,4 @@ class ParzenClassifier:
         return np.array([self._predict_instance(_.reshape(1, -1)) for _ in x])
 
     def _predict_instance(self, x):
-        return max(self.classes, key=lambda label: np.exp(self.distributions[label].score(x)))
+        return max(self.classes, key=lambda label: self.priors[label] * np.exp(self.distributions[label].score(x)))
